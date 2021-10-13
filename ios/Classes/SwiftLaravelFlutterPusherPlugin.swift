7aa0208474f7d7ff0485f8b4a5897cf2b4713cd9
Olubunmi Tosin <olubunmivictor6@gmail.com>
2021-10-13 10:25:00 +0100

    Added IOS support with fixes to pod install problems

--- ios/Classes/SwiftLaravelFlutterPusherPlugin.swift
+++ ios/Classes/SwiftLaravelFlutterPusherPlugin.swift
@@ -1,14 +1,46 @@
 import Flutter
 import UIKit
+import PusherSwift
 
 public class SwiftLaravelFlutterPusherPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "laravel_flutter_pusher", binaryMessenger: registrar.messenger())
-    let instance = SwiftLaravelFlutterPusherPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    
+    public static var eventSink: FlutterEventSink?
+    
+    public var pusherInstanceMap = [String:PusherInstance]()
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.github.olubunmitosin/pusher", binaryMessenger: registrar.messenger())
+        let instance = SwiftLaravelFlutterPusherPlugin()
+        let eventChannel = FlutterEventChannel(name: "com.github.olubunmitosin/pusherStream", binaryMessenger: registrar.messenger())
+        
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        eventChannel.setStreamHandler(StreamHandler())
+    }
+    
+    private func getPusherInstance(instanceId: String?) -> PusherInstance {
+        let id = instanceId! as String
+        if (instanceId != nil && pusherInstanceMap[id] == nil) {
+            pusherInstanceMap[id] = PusherInstance(instanceId: id)
+        }
+        return pusherInstanceMap[id]!
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let args = convertToDictionary(text: call.arguments as! String)
+        let instanceId = (args!["instanceId"] as! NSNumber).stringValue
+        let pusherInstance = getPusherInstance(instanceId: instanceId)
+        
+        pusherInstance.handle(call: call, result: result)
+    }
+}
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+func convertToDictionary(text: String) -> [String: Any]? {
+    if let data = text.data(using: .utf8) {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    return nil
 }
--- ios/laravel_flutter_pusher.podspec
+++ ios/laravel_flutter_pusher.podspec
@@ -13,17 +13,23 @@ Pod::Spec.new do |s|
   s.description      = <<-DESC
 Pusher client that supports laravel echo
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'https://github.com/olubunmitosin/laravel_flutter_pusher'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Olubunmi Tosin' => 'olubunmivictor6@gmail.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
   s.dependency 'PusherSwift', pusherLibraryVersion
   s.platform = :ios, '8.0'
 
   s.ios.deployment_target = '9.0'
-
+  s.prepare_command = <<-CMD
+    echo // Generated file, do not edit > Classes/UserAgent.h
+    echo "#define LIBRARY_VERSION @\\"#{libraryVersion}\\"" >> Classes/UserAgent.h
+    echo "#define LIBRARY_NAME @\\"laravel_flutter_pusher\\"" >> Classes/UserAgent.h
+    echo "#define PUSHER_LIBRARY_VERSION @\\"#{pusherLibraryVersion}\\"" >> Classes/UserAgent.h
+  CMD
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
